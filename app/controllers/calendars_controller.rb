a27b3b8468d59ecc30698ba02436f5ee9f26c2d8
molitomoki <molitomoki@gmail.com>
2023-03-06 19:31:14 +0900

    Issue4
    what
    requireメソッドで指定するモデル名を変更
    
    why
    正しいモデル名が指定されていなかったため

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,8 +6,11 @@ class CalendarsController < ApplicationController
     @plan = Plan.new
   end
 
+   
+
   # 予定の保存
   def create
+   
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +18,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
